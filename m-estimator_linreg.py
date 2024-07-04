a2158ba79866c1175980014c6205923b06510116
Pascal <kalezhang@hotmail.com>
2024-07-04 09:51:30 +0200

    implementation of m-estimator s-estimator mm-estimator

---
+++ m-estimator_linreg.py
@@ -0,0 +1,123 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def Huber_score_function(x, c): 
+    return np.where((np.abs(x)-c) <= 0, x ,c*np.sign(x))
+
+def Huber_weights_function(x, c):
+    return  np.where(x!=0,Huber_score_function(x, c)/x , 1)
+
+def Tukey_score_function(x, c):
+    return np.where((np.abs(x)-c) <= 0, x*(1-(x/c)**2)**2 ,0)
+
+def Tukey_weights_function(x, c):
+    non_zero = np.array(x!=0)
+    return  np.where(x!=0, Tukey_score_function(x, c)/x, 1)
+
+def m_estimator(X, y, beta_init, sigma, estimator = "", c=1, tol=1e-6, max_iter=1000):
+    n, p = X.shape
+    
+    beta = beta_init
+    res = y - X@beta
+    i=0
+    
+    match estimator:
+        case "Huber":
+            weights_function = Huber_weights_function
+        case "Tukey":
+            weights_function = Tukey_weights_function
+        case _:
+            weights_function = lambda x, _: np.ones(n)
+    
+    
+    while i < max_iter:
+        i += 1
+
+        weights = weights_function(res/sigma, c)
+        W = np.diag(weights)
+        
+        beta_new = np.linalg.pinv(X.T@W@X)@X.T@W@y
+
+        if np.linalg.norm(beta_new-beta, ord= 2) / np.linalg.norm(beta, ord= 2) < tol:
+            break
+            
+        beta = beta_new
+        res = y - X@beta
+        
+        
+    print(i)
+    return beta_new
+
+def s_estimator(X, y, beta_init, sigma, estimator = "", c=1, b=1, tol=1e-6, max_iter=1000):
+    
+    n, p = X.shape
+    
+    beta = beta_init
+    res = y - X@beta
+    i=0
+    
+    match estimator:
+        case "Huber":
+            weights_function = Huber_weights_function
+        case "Tukey":
+            weights_function = Tukey_weights_function
+        case _:
+            weights_function = lambda x, _: np.ones(n)
+    
+    
+    while i < max_iter:
+        i += 1
+
+        weights = weights_function(res/sigma, c)
+        W = np.diag(weights)
+        
+        beta_new = np.linalg.pinv(X.T@W@X)@X.T@W@y
+
+        if np.linalg.norm(beta_new-beta, ord= 2) / np.linalg.norm(beta, ord= 2) < tol:
+            break
+            
+        sigma = np.sqrt(np.sum(weights*res**2)/(n*b))
+        beta = beta_new
+        res = y - X@beta
+        
+        
+    print(i)
+    return beta_new, sigma
+
+def mm_estimator(X, y, beta_init, sigma_init, estimator = "", c=1, b=1, tol=1e-6, max_iter=1000):
+    beta = beta_init
+    i=0
+    
+    while i < max_iter:
+        i += 1
+        _, sigma = s_estimator(X, y, beta, sigma_init, estimator, c, b, tol, max_iter)
+        beta_new = m_estimator(X, y, beta, sigma_init, estimator, c, tol, max_iter)
+        
+        if np.linalg.norm(beta_new-beta, ord= 2) / np.linalg.norm(beta, ord= 2) < tol:
+            break
+        
+        beta = beta_new
+    
+    return beta_new
+        
+
+n = 10000
+p= 10
+X = np.random.normal(0, 1, (n,p+1))
+X[:,0] = 1
+
+noise = np.random.normal(0, 4, n)
+beta = np.array([i for i in range(p+1)])
+y = X@beta + noise
+
+beta_init = np.ones(p+1)
+res = y - X@beta_init
+sigma_init = 1.4826*np.median(np.abs(res-np.median(res, axis=0)), axis= 0)
+
+beta_est_m = m_estimator(X, y, beta_init, sigma_init, "Tukey", c=5)
+beta_est_s, sigma_est_s = s_estimator(X, y, beta_init, sigma_init, "Tukey", c=5)
+beta_est_mm = mm_estimator(X, y, beta_init, sigma_init, "Tukey", c=5)
+
+print(beta_est_m)
+print(beta_est_s)
+print(beta_est_mm)
